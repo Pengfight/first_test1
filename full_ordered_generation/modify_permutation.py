b649dc94cf53e3e50f02e5ea3df03f380b681de9
pengfight <815121619@qq.com>
2019-09-30 15:06:14 +0800

    添加了新的修改了字典序法

---
+++ full_ordered_generation/modify_permutation.py
@@ -0,0 +1,342 @@
+import operator
+import math
+
+
+def arr_traversing(arr):
+    for i in arr:
+        print(i, end=' ')
+    print()
+
+
+def dictionary_sort(arr, k):
+    """
+    eg: arr = [8,3,9,6,4,7,5,2,1]
+    """
+    if k < 0:
+        '''if -k > math.factorial(len(arr) + 1):
+                    arr.sort()
+                    arr_traversing(arr)
+                    return'''
+        # the number of permutations is k
+
+        for i in range(-k):
+            temp = -1
+            # each permutation, from right to left, search the first one arr[i-1] < arr[i]
+            for pos in range(len(arr) - 1, 0, -1):
+                if arr[pos] < arr[pos - 1]:
+                    temp = arr[pos - 1]
+                    break
+            if temp == -1:
+                break
+            flag = arr[pos]
+            flag_i = pos
+            # search the min number that more than arr[pos-1], from left to right
+            for pos_inline in range(pos + 1, len(arr)):
+                if temp > arr[pos_inline] > flag:
+                    flag = arr[pos_inline]
+                    flag_i = pos_inline
+                else:
+                    break
+            # swich the number
+            arr[pos - 1] = flag
+            arr[flag_i] = temp
+            '''error
+            if flag_i == len(arr) - 1:
+                continue
+            '''
+            # sort the rest numbers
+            for number in (sorted([int(i) for i in arr[pos:]], reverse=1)):
+                # print(number)
+                arr[pos] = number
+                pos += 1
+        arr_traversing(arr)
+        return
+    # covert to the number of mediators
+    temp_arr = list(range(len(arr)))
+    for i in range(len(arr)):
+        flag = 0
+        for j in range(i, len(arr), 1):
+            if arr[j] < arr[i]:
+                flag += 1
+        # print(arr[i])
+        # print(arr[i] - flag)
+        temp_arr[i] = flag
+    temp_arr.reverse()
+    # print(temp_arr)
+    # the addition of increase_method
+    if k >= 0:
+        add_num = k
+        for i in range(1, len(temp_arr)):
+            '''if temp_arr[len(temp_arr) - 1] == 0 or operator.eq([i for i in range(len(arr))], temp_arr):
+                #print('1')
+                arr_traversing([i for i in range(len(arr), 0, -1)])
+                return'''
+            # decimal = decimal
+            if add_num == 0:
+                break
+            temp_add = add_num
+            add_num = (temp_arr[i] + add_num) // (i+1)
+            temp_arr[i] = (temp_arr[i] + temp_add) % (i+1)
+            # print(add_num)
+        # print(temp_arr)
+        # reverse the arr
+        # print(temp_arr)
+        '''if temp_arr[len(temp_arr) - 1] == 0:
+            # print('1')
+            arr_traversing([i for i in range(len(arr), 0, -1)])
+            return'''
+    else:
+        sub_num = -k
+        for i in range(1, len(temp_arr)):
+            '''if temp_arr[len(temp_arr) - 1] == 0 or operator.eq([i for i in range(len(arr))], temp_arr):
+                #print('1')
+                arr_traversing([i for i in range(len(arr), 0, -1)])
+                return'''
+            # decimal = decimal
+            if sub_num == 0:
+                break
+            temp_num = sub_num
+            sub_num = temp_num // (i+1)
+            if (temp_arr[i] - temp_num % (i+1)) < 0:
+                temp_arr[i] = temp_arr[i] + (i+1) * 1 - (temp_num % (i+1))
+                for j in range(i + 1, len(temp_arr)):
+                    if temp_arr[j] - 1 < 0:
+                        temp_arr[j] = j + 1 + 1 - 1
+                        temp_arr[j + 1] = temp_arr[j + 1] - 1
+            else:
+                temp_arr[i] = temp_arr[i] - (temp_num % (i + 1))
+
+            '''temp_arr[i] = (temp_arr[i]) % (i + 1)
+            sub_num = (temp_arr[i]) // (i + 1)'''
+    temp_arr.reverse()
+    # print(temp_arr)
+    # covert to original permutation
+    # original_arr = list(range(len(arr)))
+    is_in_arr = []
+    # print(sub_arr)
+    for i in range(len(arr)):
+        permutation_num = temp_arr[i] + 1
+        # flag_sub = temp_arr[i]
+        for sub in range(len(is_in_arr)):
+            if permutation_num >= is_in_arr[sub]:
+                permutation_num += 1
+        is_in_arr.append(permutation_num)
+        is_in_arr.sort()
+        arr[i] = permutation_num
+
+    # temp_arr.reverse()
+    # print(arr)
+
+    arr_traversing(arr)
+    # print(original_arr)
+
+
+def increase_method(arr, k):
+    # covert to the number of mediators
+    temp_arr = list(range(len(arr)))
+    for i in range(len(arr)):
+        flag = 0
+        for j in range(i, len(arr), 1):
+            if arr[j] < arr[i]:
+                flag += 1
+        # print(arr[i])
+        # print(arr[i] - flag)
+        temp_arr[arr[i] - 1] = flag
+    # print(temp_arr)
+    '''if operator.eq([i for i in range(len(arr))], temp_arr):
+        arr_traversing(arr)
+        return'''
+    # the addition of increase_method
+    # print(sorted(temp_arr))
+    # temp_arr = [8, 7, 6, 5, 4, 3, 2, 0, 0]
+    # temp_arr.reverse()
+    if k >= 0:
+        add_num = k
+        for i in range(len(temp_arr)):
+            '''if temp_arr[len(temp_arr) - 1] == 0 or operator.eq([i for i in range(len(arr))], temp_arr):
+                #print('1')
+                arr_traversing([i for i in range(len(arr), 0, -1)])
+                return'''
+            # decimal = decimal
+            if add_num == 0:
+                break
+            temp_add = add_num
+            add_num = (temp_arr[i] + add_num) // (i + 1)
+            temp_arr[i] = (temp_arr[i] + temp_add) % (i + 1)
+        # print(temp_arr)
+        # reverse the arr
+        # print(temp_arr)
+        '''if temp_arr[len(temp_arr) - 1] == 0:
+            # print('1')
+            arr_traversing([i for i in range(len(arr), 0, -1)])
+            return'''
+    else:
+        sub_num = -k
+        for i in range(len(temp_arr)):
+            '''if temp_arr[len(temp_arr) - 1] == 0 or operator.eq([i for i in range(len(arr))], temp_arr):
+                #print('1')
+                arr_traversing([i for i in range(len(arr), 0, -1)])
+                return'''
+            # decimal = decimal
+            if sub_num == 0:
+                break
+            temp_num = sub_num
+            sub_num = temp_num // (i + 1)
+            if (temp_arr[i] - temp_num % (i + 1)) < 0:
+                temp_arr[i] = temp_arr[i] + (i + 1) * 1 - (temp_num % (i + 1))
+                for j in range(i + 1, len(temp_arr)):
+                    if temp_arr[j] - 1 < 0:
+                        temp_arr[j] = j + 1 - 1
+                        temp_arr[j + 1] = temp_arr[j + 1] - 1
+            else:
+                temp_arr[i] = temp_arr[i] - (temp_num % (i + 1))
+
+            '''temp_arr[i] = (temp_arr[i]) % (i + 1)
+            sub_num = (temp_arr[i]) // (i + 1)'''
+
+    temp_arr.reverse()
+    # print(temp_arr)
+
+    # covert to original permutation
+    original_arr = list(range(len(arr)))
+    sub_arr = []
+    # print(sub_arr)
+    for i in range(len(original_arr)):
+        permutation_num = len(original_arr) - i
+        flag_sub = temp_arr[i]
+        for sub in sub_arr:
+            if flag_sub >= sub:
+                flag_sub += 1
+            else:
+                break
+        sub_arr.append(flag_sub)
+        sub_arr.sort()
+        original_arr[flag_sub] = permutation_num
+    original_arr.reverse()
+    arr_traversing(original_arr)
+    # print(original_arr)
+
+
+def decrease_method(arr, k):
+    # covert to the number of mediators
+    """temp_arr = list(range(len(arr)))
+    for i in range(len(arr)):
+        flag = 1
+        for j in range(i):
+            if arr[j] < arr[i]:
+                flag += 1
+        # print(arr[i])
+        # print(arr[i] - flag)
+        temp_arr[arr[i] - 1] = arr[i] - flag"""
+    temp_arr = list(range(len(arr)))
+    for i in range(len(arr)):
+        flag = 0
+        for j in range(i, len(arr), 1):
+            if arr[j] < arr[i]:
+                flag += 1
+        # print(arr[i])
+        # print(arr[i] - flag)
+        temp_arr[arr[i] - 1] = flag
+    # print(temp_arr)
+    # reverse the arr to decrease_method
+    temp_arr.reverse()
+
+    # the addition of increase_method
+    if k >= 0:
+        add_num = k
+        for i in range(len(temp_arr)):
+            if add_num == 0:
+                break
+            temp_add = add_num
+            add_num = (temp_arr[i] + add_num) // (len(temp_arr) - i)
+            temp_arr[i] = (temp_arr[i] + temp_add) % (len(temp_arr) - i)
+        # print(temp_arr)
+        # reverse the arr
+        # temp_arr.reverse()
+        # print(temp_arr)
+    else:
+        sub_num = -k
+        for i in range(len(temp_arr)):
+            '''if temp_arr[len(temp_arr) - 1] == 0 or operator.eq([i for i in range(len(arr))], temp_arr):
+                #print('1')
+                arr_traversing([i for i in range(len(arr), 0, -1)])
+                return'''
+            # decimal = decimal
+            if sub_num == 0:
+                break
+            temp_num = sub_num
+            sub_num = temp_num // (len(temp_arr) - i)
+            if (temp_arr[i] - temp_num % (len(temp_arr) - i)) < 0:
+                temp_arr[i] = temp_arr[i] + (len(temp_arr) - i) * 1 - (temp_num % (len(temp_arr) - i))
+                for j in range(i + 1, len(temp_arr)):
+                    if temp_arr[j] - 1 < 0:
+                        temp_arr[j] = j + 1 - 1
+                        temp_arr[j + 1] = temp_arr[j + 1] - 1
+            else:
+                temp_arr[i] = temp_arr[i] - (temp_num % (len(temp_arr) - i))
+
+        # print(temp_arr)
+
+    # covert to original permutation
+    original_arr = list(range(len(arr)))
+    sub_arr = []
+    for i in range(len(original_arr)):
+        permutation_num = len(original_arr) - i
+        flag_sub = temp_arr[i]
+        for sub in sub_arr:
+            if flag_sub >= sub:
+                flag_sub += 1
+        sub_arr.append(flag_sub)
+        sub_arr.sort()
+        original_arr[flag_sub] = permutation_num
+    original_arr.reverse()
+    arr_traversing(original_arr)
+    # print(original_arr)
+
+
+if __name__ == '__main__':
+    str1_in = input()
+    n, type_method, k = [int(i) for i in str1_in.split()]
+    str2_in = input()
+    num = [int(i) for i in str2_in.split()]
+    if type_method == 1:
+        dictionary_sort(num, k)
+    elif type_method == 2:
+        increase_method(num, k)
+    elif type_method == 3:
+        decrease_method(num, k)
+    else:
+        decrease_method(num, k)
+    '''my_arr = [8, 3, 9, 6, 4, 7, 5, 2, 1]
+    my_arr1 = [3, 6, 4, 7, 5, 2, 1]
+    # dictionary_sort(my_arr, 5)
+    # increase_method(my_arr, 1)
+    decrease_method(my_arr, 0)'''
+    '''my_arr = [8, 3, 9, 6, 5, 1, 2, 4, 7]
+    my_arr2 = [9, 8, 7, 6, 5, 4, 1, 3, 2]
+    my_arr1 = [3, 6, 4, 7, 5, 2, 1]
+    # for k in range(10):
+    # dictionary_sort(my_arr, 10000000)
+    for i in range(100000):
+        increase_method(my_arr, i)'''
+    '''my_arr2 = [9, 8, 7, 6, 5, 4, 1, 3, 2]
+    # my_arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    my_arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    my_arr5 = [19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 3, 1, 2, 4]
+    my_arr3 = [1, 2, 3, 4, 5]
+    # for i in range(20):'''
+    '''increase_method(my_arr5, 5)
+    increase_method(my_arr5, 6)
+    increase_method(my_arr5, 7)
+    increase_method(my_arr5, 8)
+    increase_method(my_arr5, 9)
+    increase_method(my_arr5, 10)'''
+    # my_arr4 = [i for i in range(1, 21, 1)]
+    # dictionary_sort(my_arr4, math.factorial(20)-1)
+    '''for k in range(1, 21):
+        my_arr4 = [i for i in range(k, 0, -1)]
+        # increase_method(my_arr4, -10000)
+        increase_method(my_arr4, -math.factorial(k) + 1)'''
+
+
+
